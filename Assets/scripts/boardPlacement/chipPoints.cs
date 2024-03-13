7cab1e9565af0095e0253f6ef8faa7c82483cded
BeckBob <unsworthbecky@gmail.com>
2024-03-13 14:07:44 +0000

    added more rules

--- Assets/scripts/boardPlacement/chipPoints.cs
+++ Assets/scripts/boardPlacement/chipPoints.cs
@@ -13,7 +13,6 @@ public class chipPoints : MonoBehaviour
 {
     private Program programInstance;
 
-    private ingredientEffects _ingredientEffects;
 
     public UnityEvent OnScoreChanged;
 
@@ -32,34 +31,88 @@ public class chipPoints : MonoBehaviour
 
     List<string> ingredientsList = new List<string>();
 
-    public int mushroomRule = 1;
+    public int mushroomRule = 3;
+    public int mandrakeRule = 1;
 
     public int extraPoints;
 
     public int extraRubies = 0;
 
+    private int pumpkins = 0;
+
     public void OnTriggerEnter(Collider other)
     {
         ingredientsList.Add(other.gameObject.tag);
         extraPoints = 0; extraRubies = 0;
-        if (other.gameObject.tag.Contains("mushroom") && mushroomRule == 1 && ingredientsList.Count >= 2 && ingredientsList[ingredientsList.Count - 2].Contains("cherryBomb"))
+        if (mushroomRule == 1)
         {
+            if (other.gameObject.tag.Contains("mushroom") && ingredientsList.Count >= 2 && ingredientsList[ingredientsList.Count - 2].Contains("cherryBomb"))
+            {
 
-            if (ingredientsList[ingredientsList.Count - 2].Contains("One"))
+                if (ingredientsList[ingredientsList.Count - 2].Contains("One"))
+                {
+                    extraPoints = 1;
+                }
+                else if (ingredientsList[ingredientsList.Count - 2].Contains("Two"))
+                {
+                    extraPoints = 2;
+                }
+                else if (ingredientsList[ingredientsList.Count - 2].Contains("Three"))
+                {
+                    extraPoints = 3;
+                }
+                UnityEngine.Debug.Log(extraPoints);
+            }
+        }
+
+        else if (mushroomRule == 2)
+        {
+            if (other.gameObject.tag.Contains("cherryBombOne") && (ingredientsList.Contains("mushroomOne") || ingredientsList.Contains("mushroomTwo") || ingredientsList.Contains("mushroomFour")))
             {
                 extraPoints = 1;
+                UnityEngine.Debug.Log(extraPoints);
             }
-            else if (ingredientsList[ingredientsList.Count - 2].Contains("Two"))
+        }
+
+        else if (mushroomRule == 3)
+        {
+            if (other.gameObject.CompareTag("pumpkinOne"))
+            { pumpkins += 1; }
+            UnityEngine.Debug.Log(pumpkins);
+            if (other.gameObject.tag.Contains("mushroom") && pumpkins < 3)
+                {
+                extraPoints = 1;
+            }
+            if (other.gameObject.tag.Contains("mushroom") && pumpkins >= 3)
             {
                 extraPoints = 2;
             }
-            else if (ingredientsList[ingredientsList.Count - 2].Contains("Three"))
-            {
-                extraPoints = 3;
-            }
-            UnityEngine.Debug.Log(extraPoints);
         }
-        
+        //mushroom rule 3 put mushroom chip to side SO remove from end of array put in special pot that gets called at the end of the round and player gets given a yes or no question to put it on - also maybe put in thing to show what the player gets if they move one more space
+
+        if (mandrakeRule == 1)
+        {
+            if (ingredientsList.Count >= 2 && ingredientsList[ingredientsList.Count - 2].Contains("mandrake"))
+            {
+                if (other.gameObject.tag.Contains("One"))
+                {
+                    extraPoints = 1;
+                }
+                else if (other.gameObject.tag.Contains("Two"))
+                {
+                    extraPoints = 2;
+                }
+                else if(other.gameObject.tag.Contains("Three"))
+                {
+                    extraPoints = 3;
+                }
+                else if (other.gameObject.tag.Contains("Four"))
+                {
+                    extraPoints = 4;
+                }
+            }
+        }
+
         Chips[] boardPlacement = programInstance.GetBoardPlacement();
      
 
